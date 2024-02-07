78acd9c0573da792e033bf336a4de8d5c20e377e
harry.xie <harry.xie@iherb.com>
2024-02-07 10:16:34 -0800

    Initial version

---
+++ KafkaConsumerApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Confluent.Kafka;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = "test-consumer-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+        {
+            consumer.Subscribe("sqlserver.dbo.SampleTable");
+
+            try
+            {
+                while (true)
+                {
+                    var message = consumer.Consume();
+                    Console.WriteLine($"Received message: {message.Value}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                consumer.Close();
+            }
+        }
+    }
+}
